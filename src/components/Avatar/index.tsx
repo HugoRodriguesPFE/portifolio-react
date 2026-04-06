df68b86652a350d84b19668e359c81f29f0e044a
hugo <hugorodriguesbmwg@gmail.com>
2026-04-06 11:04:54 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,7 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => (
+  <FotoPerfil src="https://github.com/HugoRodriguesPFE.png" />
+)
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,19 @@
 import Titulo from '../../components/Titulo'
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="http://github.com/HugoRodriguesPFE.png" />
-    <Titulo fontSize={20}>Hugo Rodrigues</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Hugo Rodrigues</Titulo>
+      <Paragrafo tipo="secundario">HugoRodriguesPFE</Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
